b441062acc49d6c87f6e17dcf3e9a7a86c8da2ac
Mehmet Selim CAN <MehmetSelimCN@hotmail.com>
2023-06-20 21:21:33 +0300

    Editor script build issue fixed

--- Deliver Shapes/Assets/Editor/NodeDataEditor.cs
+++ Deliver Shapes/Assets/Editor/NodeDataEditor.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
-using System.Net.NetworkInformation;
 
 [CustomEditor(typeof(NodeData))]
 public class NodeDataEditor : Editor {
